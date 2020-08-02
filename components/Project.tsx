ed2c605a1ffab02d4a7d70bfa5eaae9f4b2f2411
Fabian <fabian.paul.gurtner@gmail.com>
2020-08-02 22:51:38 +0200

    Use Bootstrap Cards to display projects

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import {Projects} from "../types/projects";
+import {Project as ProjectData} from "../types/projects";
+import Card from "react-bootstrap/Card";
 
 interface ProjectProps {
-	data: Projects["projects"][0]
+	data: ProjectData
 }
 
-const Project: React.FC<ProjectProps> = ({data: {title, teaser}}) => {
+const Project: React.FC<ProjectProps> = ({data: {title, teaser, language, links}}) => {
+	const linkElements: JSX.Element[] = links.map((link, key) => {
+		switch (link.type) {
+			case "github":
+				return <Card.Link key={key} href={"https://github.com/" + link.link}>GitHub</Card.Link>;
+			case "gitlab":
+				return <Card.Link key={key} href={"https://gitlab.com/" + link.link}>GitLab</Card.Link>;
+		}
+	});
+
 	return (
-		<div style={{height: 400, width: 250, backgroundColor: "gray"}} className="mb-5">
-			<h3>{title}</h3>
-			<p>{teaser}</p>
-		</div>
+		<Card body style={{marginBottom: 5, width: "18rem"}}>
+			<Card.Title>{title}</Card.Title>
+			<Card.Subtitle className="mb-2 text-muted">{language.name}</Card.Subtitle>
+			<Card.Text>{teaser}</Card.Text>
+			{linkElements}
+		</Card>
 	);
 };
 
---
+++ types/projects.ts
@@ -0,0 +1,64 @@
+export enum ProjectState {
+	TODO,
+	WIP,
+	Finished,
+	OnHold,
+	Abandoned
+}
+
+export interface Technology {
+	id: string;
+	name: string;
+}
+
+export interface Language {
+	id: string;
+	name: string;
+}
+
+export interface ProjectLink {
+	type: string,
+	link: string
+}
+
+interface ProjectCommon {
+	id: string,
+	title: string,
+	teaser: string,
+	description: string,
+	related: string[],
+	tags: string[],
+}
+
+export interface Project extends ProjectCommon {
+	links: ProjectLink[],
+	state: ProjectState,
+	startDate: Date,
+	endDate: Date,
+	usedTechnologies: Technology[],
+	language: Language
+}
+
+export interface RawProject extends ProjectCommon {
+	links: {
+		type: string,
+		link: string
+	}[],
+	state: string,
+	startDate: string,
+	endDate: string,
+	usedTechnologies: string[],
+	language: string
+}
+
+export interface ProjectData {
+	projects: Project[];
+	technologies: Technology[];
+	languages: Language[];
+}
+
+export interface RawProjectData {
+	projects: RawProject[];
+	technologies: Technology[];
+	languages: Language[];
+}
